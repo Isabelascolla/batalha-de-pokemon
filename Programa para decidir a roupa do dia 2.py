363c95b7e14e27b35c12517c034d39bef4f6199f
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-12-15 19:21:44 -0300

    Add files via upload

---
+++ Programa para decidir a roupa do dia 2.py
@@ -0,0 +1,13 @@
+# Programa para decidir a roupa do dia
+
+frio=("casaco, calca, tenis, meia")
+quente=("camiseta, bermuda, chinelo")
+chuva=("calca, tenis, camiseta, guarda chuva")
+     
+
+if frio: 
+    print("usar casaco, calca, tenis, meia")
+if quente:
+    print ("camiseta, bermuda, chinelo")
+else:
+    print("calca, tenis, camiseta, guarda chuva")
